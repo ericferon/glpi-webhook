a69115a38b1b079560601ca143a91710f9edc97d
Leewan Meneses <leewanmeneses@kedu.com.br>
2025-07-24 20:35:26 -0300

    Fix: Webhook URL encoding causing 403 errors with external APIs

--- inc/notificationeventwebhook.class.php
+++ inc/notificationeventwebhook.class.php
@@ -158,7 +158,7 @@ static public function extraRaise($params) {
 							$key = $webhook_infos[static::getTargetFieldName()];
 							$url = $webhook_infos['additionnaloption']['address']; 
 							$url = NotificationTemplate::process($webhook_infos['additionnaloption']['address'], $data); // substitute variables in url
-							$url = str_replace(["\n", "\r", "\t"], ['', '', ''], htmlentities($url)); // translate HTML-significant characters and suppress remaining escape characters
+							$url = str_replace(["\n", "\r", "\t"], ['', '', ''], $url); // translate HTML-significant characters and suppress remaining escape characters
 							if ($template_datas = $template->getByLanguage($webhook_infos['language']))
 							{
 								$template_datas  = Sanitizer::unsanitize($template_datas); // unescape html from DB
